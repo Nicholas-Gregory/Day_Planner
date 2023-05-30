1cbb7e7d114402b8d34c2a596ac6581b7fcf2368
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-05-30 13:41:18 -0700

    Added functionality to display any text the user has previously saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,26 +52,16 @@ function colorHours(element, time) {
   })
 }
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+function displayText(element) {
+  element.children().each(function() {
+    $(this).children("textarea").val(localStorage.getItem($(this).attr("id")));
+  });
+}
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+$(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
   // Display current day
   $("#currentDay").text(dayjs().format("dddd MMMM DD"));
@@ -81,6 +71,9 @@ $(function () {
   displayHours(hours);
   colorHours(hours, dayjs());
 
+  // Display saved text
+  displayText(hours);
+
   // Add event listeners to save buttons
   hours.children().each(function() {
     $(this).children("button").on("click", function() {
@@ -89,5 +82,5 @@ $(function () {
 
       localStorage.setItem(String(id), text);
     })
-  })
+  });
 });
